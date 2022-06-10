cd78617e904a5af532491eff9ff7d43f10c67805
ingvarzharov <ingvarzharov@yandex.ru>
2022-06-10 16:50:12 +0500

    first commit

---
+++ laravel/resources/views/dashboard.blade.php
@@ -0,0 +1,208 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <div class="col-xl-12 mb-5">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <h3 class="mb-0">Товары (Топ 10)</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <!-- Projects table -->
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Название</th>
+                                    <th scope="col">Ссылка</th>
+                                    <th scope="col">Дата создания</th>
+                                    <th scope="col">Дата обновления</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">
+                                        /argon/
+                                    </th>
+                                    <td>
+                                        4,569
+                                    </td>
+                                    <td>
+                                        340
+                                    </td>
+                                    <td>
+                                        <i class="fas fa-arrow-up text-success mr-3"></i> 46,53%
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        /argon/index.html
+                                    </th>
+                                    <td>
+                                        3,985
+                                    </td>
+                                    <td>
+                                        319
+                                    </td>
+                                    <td>
+                                        <i class="fas fa-arrow-down text-warning mr-3"></i> 46,53%
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        /argon/charts.html
+                                    </th>
+                                    <td>
+                                        3,513
+                                    </td>
+                                    <td>
+                                        294
+                                    </td>
+                                    <td>
+                                        <i class="fas fa-arrow-down text-warning mr-3"></i> 36,49%
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        /argon/tables.html
+                                    </th>
+                                    <td>
+                                        2,050
+                                    </td>
+                                    <td>
+                                        147
+                                    </td>
+                                    <td>
+                                        <i class="fas fa-arrow-up text-success mr-3"></i> 50,87%
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        /argon/profile.html
+                                    </th>
+                                    <td>
+                                        1,795
+                                    </td>
+                                    <td>
+                                        190
+                                    </td>
+                                    <td>
+                                        <i class="fas fa-arrow-down text-danger mr-3"></i> 46,53%
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-12">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <h3 class="mb-0">Категории (Топ 10)</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <!-- Projects table -->
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Название</th>
+                                <th scope="col">Ссылка</th>
+                                <th scope="col">Дата создания</th>
+                                <th scope="col">Дата обновления</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">
+                                    /argon/
+                                </th>
+                                <td>
+                                    4,569
+                                </td>
+                                <td>
+                                    340
+                                </td>
+                                <td>
+                                    <i class="fas fa-arrow-up text-success mr-3"></i> 46,53%
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    /argon/index.html
+                                </th>
+                                <td>
+                                    3,985
+                                </td>
+                                <td>
+                                    319
+                                </td>
+                                <td>
+                                    <i class="fas fa-arrow-down text-warning mr-3"></i> 46,53%
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    /argon/charts.html
+                                </th>
+                                <td>
+                                    3,513
+                                </td>
+                                <td>
+                                    294
+                                </td>
+                                <td>
+                                    <i class="fas fa-arrow-down text-warning mr-3"></i> 36,49%
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    /argon/tables.html
+                                </th>
+                                <td>
+                                    2,050
+                                </td>
+                                <td>
+                                    147
+                                </td>
+                                <td>
+                                    <i class="fas fa-arrow-up text-success mr-3"></i> 50,87%
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    /argon/profile.html
+                                </th>
+                                <td>
+                                    1,795
+                                </td>
+                                <td>
+                                    190
+                                </td>
+                                <td>
+                                    <i class="fas fa-arrow-down text-danger mr-3"></i> 46,53%
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+    </div>
+@endsection
+
+@push('js')
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+@endpush
